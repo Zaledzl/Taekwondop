81feabf2ad9edee0497e0fe7ddd4547a7dd6504c
Zale <624343835@qq.com>
2020-05-13 09:28:51 +0800

    Initial commit

---
+++ app/src/main/java/com/example/taekwondop/FightActivity.java
@@ -0,0 +1,14 @@
+package com.example.taekwondop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FightActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fight);
+    }
+}
---
+++ app/src/main/java/com/example/taekwondop/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.taekwondop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button main_fight,main_help,main_setting,main_bluetooth,main_record;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        ////onCreate 是启动
+        super.onCreate(savedInstanceState);
+        //设置界面的形式
+        setContentView(R.layout.activity_main);
+        main_bluetooth = findViewById(R.id.main_bluetooth);
+        main_setting = findViewById(R.id.main_setting);
+        main_help= findViewById(R.id.main_help);
+        main_fight = findViewById(R.id.main_fight);
+        main_record = findViewById(R.id.main_record);
+        //先根据控件ID找到控件，然后调用setListener
+        setListener();
+    }
+
+    //设置监听器 给每一个事件设置点击事件
+    private void setListener(){
+        //onclick实现了OnClick类
+        OnClick onclick = new OnClick();
+        main_fight.setOnClickListener(onclick);
+        main_help.setOnClickListener(onclick);
+        main_setting.setOnClickListener(onclick);
+        main_bluetooth.setOnClickListener(onclick);
+        main_record.setOnClickListener(onclick);
+
+    }
+
+    //OnClick 实现了 ONClickListener接口
+    private class OnClick implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v){
+            Intent intent = null;
+            switch (v.getId()){
+                case R.id.main_bluetooth:
+                    intent = new Intent(MainActivity.this,BluetoothActivity.class);
+                    break;
+                case R.id.main_fight:
+                    intent = new Intent(MainActivity.this,FightActivity.class);
+                    break;
+                case R.id.main_help:
+                    intent = new Intent(MainActivity.this,HelpActivity.class);
+                    break;
+                case R.id.main_setting:
+                    intent = new Intent(MainActivity.this, SettingActivity.class);
+                    break;
+                case R.id.main_record:
+                    intent = new Intent(MainActivity.this, RecordActivity.class);
+                    break;
+            }
+            startActivity(intent);
+        }
+    }
+
+}
